af6d6bbe25130b8df2678abe7570c4eaa00689bd
michal <kmitmichal@gmail.com>
2025-04-05 11:04:10 +0200

    express

--- server/src/rateLimit.mjs
+++ server/src/rateLimit.mjs
@@ -1,6 +1,6 @@
 import rateLimit from 'express-rate-limit';
 export const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minut
-    max: 100, // max 100 požadavků
+    windowMs: 5 * 60 * 1000, // 15 minut
+    max: 1000, // max 100 požadavků
     message: "Příliš mnoho požadavků, zkuste to za chvíli"
 });
